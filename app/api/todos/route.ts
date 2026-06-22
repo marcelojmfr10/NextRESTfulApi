646b2d39c5d2d02579aab389944a09f2c905f211
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-06-21 20:01:25 -0600

    Paginación simple

---
+++ app/api/todos/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+  if (isNaN(take)) {
+    return Response.json(
+      { message: `Take tiene que ser un número` },
+      { status: 400 },
+    );
+  }
+
+  if (isNaN(skip)) {
+    return Response.json(
+      { message: `Skip tiene que ser un número` },
+      { status: 400 },
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
+
+  return Response.json(todos);
+}
