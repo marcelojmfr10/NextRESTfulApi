94eea494e835777ff6492eb0003bcdb12e5ef56f
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-07-01 20:33:54 -0600

    Ejemplo básico de cookies

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { SidebarItem } from "./SidebarItem";
 import {
   IoCalendarOutline,
   IoCheckboxOutline,
+  IoCodeDownloadOutline,
   IoListOutline,
 } from "react-icons/io5";
 
@@ -23,6 +24,11 @@ const menuItems = [
     icon: <IoListOutline />,
     title: "Server Actions",
     path: "/dashboard/server-todos",
+  },
+  {
+    icon: <IoCodeDownloadOutline />,
+    title: "Cookies",
+    path: "/dashboard/cookies",
   },
 ];
 
