81a22a6ccd18d47af5417aee886a7ad0c9634842
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-07-01 21:14:08 -0600

    Agregar y eliminar productos al carrito

--- app/products/components/ProductCard.tsx
+++ app/products/components/ProductCard.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
 import { Star } from "./Star";
+import {
+  addProductToCart,
+  removeProductFromCart,
+} from "@/app/shopping-cart/actions/actions";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id: string;
@@ -11,6 +18,17 @@ interface Props {
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
+  const router = useRouter();
+  const onAddToCart = () => {
+    addProductToCart(id);
+    router.refresh();
+  };
+
+  const onRemoveProductFromCart = () => {
+    removeProductFromCart(id);
+    router.refresh();
+  };
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       {/* Product Image */}
@@ -47,15 +65,21 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
 
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            ${price}
+          <span className="text-2xl font-bold text-gray-900 dark:text-white">
+            ${price.toFixed(2)}
           </span>
 
           <div className="flex">
-            <button className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              onClick={onAddToCart}
+              className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
               <IoAddCircleOutline size={25} />
             </button>
-            <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+            <button
+              onClick={onRemoveProductFromCart}
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            >
               <IoTrashOutline size={20} />
             </button>
           </div>
